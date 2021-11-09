0e0148fc5fa89c22fe015dbbda07c320e36d3b08
LarisaGimbu <gimbularisa@gmail.com>
2021-11-09 15:53:00 +0100

    getRandomNumber

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@
   Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
  */
 
+const boxNumber = document.getElementById('box-number');
+
+for(let i=0; i<5; i++){
+
+  const spanElement = document.createElement('span');
+
+  spanElement.className = 'me-3';
+
+  boxNumber.append(spanElement);
+  spanElement.innerHTML += getRandomNumber(1,100);
+
+  console.log(spanElement);
+}
+
+
+function getRandomNumber(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
